bc8bcc33acabd892d310f6998af4e7876c14581f
Vinicius Ouro Preto Ribeiro <viniciusouro73@gmail.com>
2023-11-29 09:25:00 -0300

    Criando a estrutura do blog

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
 # Create your views here.
 
+posts = list(range(30))
+
 def index(request):
-    return render(request, 'blog/pages/index.html')
+    paginator = Paginator(posts, 6)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'blog/pages/index.html', {'page_obj': page_obj, })
 
 def page(request):
+    paginator = Paginator(posts, 6)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
     return render(request, 'blog/pages/page.html')
 
 def post(request):
+    paginator = Paginator(posts, 6)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
     return render(request, 'blog/pages/post.html')
